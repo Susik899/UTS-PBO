b2c9bfa5633af42e810946d47c6b77d14f37437e
FerdyMahardhika <Oneilkerrylaurent1905@gmail.com>
2025-11-06 09:57:32 +0800

    first commit

---
+++ UTS.js
@@ -0,0 +1,52 @@
+class Cuti {
+  constructor(jenis, durasi, maxKuota) {
+    this.jenis = jenis;
+    this.durasi = durasi;
+    this.maxKuota = maxKuota;
+  }
+
+  ajukanCuti() {
+    console.log(`Mengajukan ${this.jenis}`);
+    console.log(`Durasi: ${this.durasi} hari`);
+    console.log(`Max Kuota: ${this.maxKuota} hari`);
+    
+    if (this.maxKuota >= this.durasi) {
+      console.log(`Status: Pengajuan berhasil!`);
+    } else {
+      console.log("Status: Kuota anda tidak cukup untuk mengajukan cuti ini");
+    }
+  }
+}
+
+class CutiTahunan extends Cuti {
+  constructor(durasi) {
+    super("Cuti Tahunan", durasi, 12);
+  }
+}
+
+class CutiSakit extends Cuti {
+  constructor(durasi) {
+    super("Cuti Sakit", durasi, 2);
+  }
+}
+
+class CutiMelahirkan extends Cuti {
+  constructor(durasi) {
+    super("Cuti Melahirkan", durasi, 90);
+  }
+}
+
+const cutiTahunan = new CutiTahunan(10); 
+cutiTahunan.ajukanCuti();
+
+const cutiSakit = new CutiSakit(1);
+cutiSakit.ajukanCuti();
+
+const cutiMelahirkan = new CutiMelahirkan(90);
+cutiMelahirkan.ajukanCuti();
+
+const cutiTahunanLebih = new CutiTahunan(15); 
+cutiTahunanLebih.ajukanCuti();
+
+const cutiSakitLebih = new CutiSakit(3);
+cutiSakitLebih.ajukanCuti();
